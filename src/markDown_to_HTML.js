42fb5a730e507dcb59d50f98b036ff0553b80407
bvasquezm <bvasquezm@uc.cl>
2024-03-25 01:06:58 -0300

    fix(mardDownToHtml): SIMPLIFY main function

--- src/markDown_to_HTML.js
+++ src/markDown_to_HTML.js
@@ -6,24 +6,15 @@ const markDown_to_HTML = (markdown) => {
     const fused_elements = parser.fuse_same_type_elements(elements);
     let html = '';
 
-    console.log(fused_elements);
-
     for (let i = 0; i < fused_elements.length; i++) {
-        const element = fused_elements[i];
         let html_element;
+        const element = fused_elements[i];
 
-        html_element = function_resolver.apply_consecutive_formats(element)
-
-        console.log(html_element);
-
+        html_element = function_resolver.apply_format(element);
         html += html_element;
     }
 
     return html;
 }
 
-if (require.main === module) {
-    console.log('Testing markDown_to_HTML.js');
-    const input_string = "* Hola \n * **Como estas** \n * Bien y tu";
-    console.log(markDown_to_HTML(input_string));
-}
+module.exports = markDown_to_HTML;
