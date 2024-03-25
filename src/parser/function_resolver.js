89839b9fb272245379118124ee11e55c3ceca02c
Cristóba Pérez-Cotapos <cprezcotapos@uc.cl>
2024-03-25 09:20:48 -0300

    links arreglados

--- src/parser/function_resolver.js
+++ src/parser/function_resolver.js
@@ -1,4 +1,4 @@
-const { compose } = require('../converter/utils.js');
+const { compose, compose_3 } = require('../converter/utils.js');
 const format_backticks = require('../converter/backticks.js');
 const format_code_blocks = require('../converter/code_block.js');
 const emphasis_converter = require('../converter/emphasis.js');
@@ -6,7 +6,8 @@ const header_converter = require('../converter/headers.js');
 const formatImages = require('../converter/image.js');
 const paragraph_converter = require('../converter/paragraphs.js');
 const lists_converter = require('../converter/lists.js');
-const hyperlinks = require('../converter/hyperlink.js');
+const hyperlinks = require('../converter/hyperlink.js')
+
 
 const function_resolver = (type) => {
     if (type === 'header') {
@@ -30,15 +31,12 @@ const function_resolver = (type) => {
     if (type === 'paragraph') {
         return paragraph_converter.paragraph_converter;
     }
-    if (type === 'hyperlink') {
-        return hyperlinks.hyperlink_converter;
-    }
 }
 
 const apply_format = (element) => {
     const { type, text } = element;
     const resolvedFunction = function_resolver(type);
-    const formatted_element = compose(resolvedFunction, emphasis_converter.emphasis_converter)(text);
+    const formatted_element = compose_3(hyperlinks.hyperlink_converter, emphasis_converter.emphasis_converter, resolvedFunction)(text);
     return formatted_element;
 }
 
