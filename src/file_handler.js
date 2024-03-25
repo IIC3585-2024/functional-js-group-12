47615781919573ef8b9c60e047aa68c692f7787d
scavagnaro <sebastian@xpendit.com>
2024-03-25 10:49:46 -0300

    feat: :sparkles: added input path line

--- src/file_handler.js
+++ src/file_handler.js
@@ -1,21 +1,36 @@
 const fs = require('fs');
+const readLine = require('node:readline');
 const markDown_to_HTML = require('./markDown_to_HTML.js');
 
-fs.readFile('./src/readme.md', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+// input path
+
+let inputPath =  readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+inputPath.question('Enter the path of the .md file you want to convert to HTML: ', (path) => {
+    console.log(`Path is ${path}`);
+    inputPath.close();
 
 
-    const html = markDown_to_HTML(data);
+    fs.readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
 
-    fs.writeFile('./src/output.html', html, (err) => {
-        if (err) {
-            console.error(err);
-            return;
+
+            const html = markDown_to_HTML(data);
+
+            fs.writeFile('./src/output.html', html, (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+                console.log('File has been written');
+            });
         }
-        console.log('File has been written');
-    });
-}
+    );
+    }
 );
